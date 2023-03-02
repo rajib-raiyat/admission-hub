e7441a63951d14d7bf6f9e123a2160195b376775
Komol Kunty Rajib <rajib@quantummethod.org.bd>
2023-03-02 15:13:22 +0600

    Assets folder added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='front-end/templates')
+app = Flask(__name__, template_folder='front-end/templates', static_folder='front-end/assets')
 
 
 @app.route("/")
