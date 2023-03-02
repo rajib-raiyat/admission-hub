d6d8e8bfe234abfc7b48d8d7947712d7e8bfb956
Komol Kunty Rajib <rajib@quantummethod.org.bd>
2023-03-02 14:55:24 +0600

    Basic Bootstrap setup

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='front-end/templates')
 
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
