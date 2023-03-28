19fa04675078f79a715492446aaea8b4ba6944f8
Tanvir Mobasshir <mdtanvirmobasshir@gmail.com>
2023-03-28 15:56:43 +0600

    created university data

--- database/models.py
+++ database/models.py
@@ -1,6 +1,5 @@
-from sqlalchemy import create_engine, Column, Integer, String, Text, Boolean, Date, ForeignKey
+from sqlalchemy import Column, Integer, Text, Date, VARCHAR
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, sessionmaker
 
 from database import engine
 
@@ -10,7 +9,7 @@ Base = declarative_base()
 class Applicants(Base):
     __tablename__ = 'applicants'
 
-    applicant_id = Column(Text, primary_key=True)
+    applicant_id = Column(VARCHAR(36), primary_key=True)
     hsc_roll = Column(Text)
     hsc_reg = Column(Text)
     hsc_board = Column(Text)
@@ -29,7 +28,7 @@ class Applicants(Base):
 class University(Base):
     __tablename__ = 'universities'
 
-    university_id = Column(Text, primary_key=True)
+    university_id = Column(VARCHAR(36), primary_key=True)
     university_name = Column(Text)
     university_image = Column(Text)  # abs path of the image
     university_intro = Column(Text)
@@ -38,8 +37,8 @@ class University(Base):
 class AdmissionGroup(Base):
     __tablename__ = 'admission_groups'
 
-    admission_group_id = Column(Text, primary_key=True)
-    university_id = Column(Text)
+    admission_group_id = Column(VARCHAR(36), primary_key=True)
+    university_id = Column(VARCHAR(36))
     group_name = Column(Text)
     exam_datetime = Column(Text)  # datetime
     application_fee = Column(Integer)
@@ -48,9 +47,9 @@ class AdmissionGroup(Base):
 class Payment(Base):
     __tablename__ = 'payment'
 
-    transaction_id = Column(Text, primary_key=True)
-    applicant_id = Column(Text)
-    student_id = Column(Text)
+    transaction_id = Column(VARCHAR(36), primary_key=True)
+    applicant_id = Column(VARCHAR(36))
+    student_id = Column(VARCHAR(36))
     payment_amount = Column(Text)
     payment_status = Column(Text)
     payment_time = Column(Date)
@@ -59,29 +58,29 @@ class Payment(Base):
 class Application(Base):
     __tablename__ = 'application'
 
-    application_id = Column(Text, primary_key=True)
-    applicant_id = Column(Text)
+    application_id = Column(VARCHAR(36), primary_key=True)
+    applicant_id = Column(VARCHAR(36))
     status = Column(Text)
-    university_id = Column(Text)
-    admission_group_id = Column(Text)
-    transaction_id = Column(Text)
+    university_id = Column(VARCHAR(36))
+    admission_group_id = Column(VARCHAR(36))
+    transaction_id = Column(VARCHAR(36))
 
 
 class Exam(Base):
     __tablename__ = 'exam'
 
-    exam_id = Column(Text)
-    venue_id = Column(Text)
+    exam_id = Column(VARCHAR(36), primary_key=True)
+    venue_id = Column(VARCHAR(36))
     venue_name = Column(Text)
     students = Column(Text)  # list
     applicantions = Column(Text)  # list
-    university_id = Column(Text)
-    admission_group_id = Column(Text)
-
-
-# Base.metadata.create_all(engine)
+    university_id = Column(VARCHAR(36))
+    admission_group_id = Column(VARCHAR(36))
 
 
 if __name__ == '__main__':
-    Applicants.__table__.drop(engine)
-    Applicants.__table__.create(engine)
+    Base.metadata.create_all(engine)
+
+    for i in [Applicants, University, AdmissionGroup, Payment, Application, Exam]:
+        i.__table__.drop(engine)
+        i.__table__.create(engine)
