1d9e7226f63e90d31f984b7cfd0755aa59452e7d
Komol Kunty Rajib <rajib@quantummethod.org.bd>
2023-04-29 18:24:32 +0600

    Applicant image save payment status update

--- database/dataset.py
+++ database/dataset.py
@@ -2,11 +2,11 @@ import random
 import string
 import uuid
 from datetime import timedelta, datetime
+from random import randint
 from uuid import uuid4
 
 from database import session
 from database.models import Applicants, University, AdmissionGroup
-from random import randint
 
 
 def random_with_n_digits(n):
@@ -49,7 +49,7 @@ university = [
     {
         'university_id': 'f9606f18-52c2-4664-8f1c-be2db74a9e17',
         'university_name': 'Bangabandhu Sheikh Mujibur Rahman Science and Technology University',
-        'university_image': f'../assets/university_image//f9606f18-52c2-4664-8f1c-be2db74a9e17.jpg',
+        'university_image': f'../assets/university_image/f9606f18-52c2-4664-8f1c-be2db74a9e17.jpg',
         'university_intro': 'BSMRSTU, known as Bangabandhu Sheikh Mujibur Rahman Science and Technology University, is a public university '
                             'in Gopalganj. The institution was founded in 2001 with an emphasis on education and research in science and '
                             'technology. With numerous active research initiatives and partnerships with other universities and research '
@@ -58,7 +58,7 @@ university = [
     {
         'university_id': 'cddf733e-6e9c-45af-8553-cc266e6aca2d',
         'university_name': 'University of Dhaka',
-        'university_image': f'../assets/university_image//cddf733e-6e9c-45af-8553-cc266e6aca2d.jpg',
+        'university_image': f'../assets/university_image/cddf733e-6e9c-45af-8553-cc266e6aca2d.jpg',
         'university_intro': 'Dhaka University is a renowned institution of higher education in South Asia, known for its academic excellence,'
                             'research capabilities, and cultural significance. It offers undergraduate and postgraduate programs in various '
                             'fields of study, including arts, sciences, business, engineering, law, and medicine.'
@@ -66,7 +66,7 @@ university = [
     {
         'university_id': '6fcb1eb8-946a-41f9-8c98-2f0d8a5f2a3e',
         'university_name': 'University of Rajshahi',
-        'university_image': f'../assets/university_image//6fcb1eb8-946a-41f9-8c98-2f0d8a5f2a3e.jpg',
+        'university_image': f'../assets/university_image/6fcb1eb8-946a-41f9-8c98-2f0d8a5f2a3e.jpg',
         'university_intro': 'The University of Rajshahi is one of the largest public co-educational research university in Bangladesh situated near '
                             'the northern region. It is known for its active research culture, with several research centers and institutes, and '
                             'has made significant contributions to the advancement of knowledge in various fields.'
@@ -74,7 +74,7 @@ university = [
     {
         'university_id': '3977ab12-cc17-42ec-83f0-b752802761e5',
         'university_name': 'University of Chittagong',
-        'university_image': f'../assets/university_image//3977ab12-cc17-42ec-83f0-b752802761e5.jpg',
+        'university_image': f'../assets/university_image/3977ab12-cc17-42ec-83f0-b752802761e5.jpg',
         'university_intro': 'Chittagong University is one of the most prestigious and oldest institutions of higher learning in Bangladesh.'
                             'The institution is known for its beautiful campus, academic excellence, research facilities, and contribution '
                             'to the development of higher education in Bangladesh.'
@@ -82,7 +82,7 @@ university = [
     {
         'university_id': '77d1387c-0654-41a5-beef-9ec77fcb31f4',
         'university_name': 'Jahangirnagar University',
-        'university_image': f'../assets/university_image//77d1387c-0654-41a5-beef-9ec77fcb31f4.jpg',
+        'university_image': f'../assets/university_image/77d1387c-0654-41a5-beef-9ec77fcb31f4.jpg',
         'university_intro': 'Jahangirnagar University is a prestigious public research university located in Savar, Bangladesh. Established in 1970, '
                             'the university is named after the Mughal Emperor Jahangir. Jahangirnagar University is known for its picturesque campus, '
                             'vibrant student community, and strong commitment to social justice and environmental sustainability.'
@@ -90,7 +90,7 @@ university = [
     {
         'university_id': '5b7a2ba2-4a4d-44f8-81eb-41003c9fbd58',
         'university_name': 'Khulna University',
-        'university_image': f'../assets/university_image//5b7a2ba2-4a4d-44f8-81eb-41003c9fbd58.jpg',
+        'university_image': f'../assets/university_image/5b7a2ba2-4a4d-44f8-81eb-41003c9fbd58.jpg',
         'university_intro': 'Khulna University is a public university located in Khulna, a southwestern city of Bangladesh. The university offers '
                             'undergraduate and postgraduate programs in various fields of study, including science, engineering, social sciences, and'
                             ' humanities. '
@@ -98,7 +98,7 @@ university = [
     {
         'university_id': '9855d5ef-16a1-4ed0-b322-0426791efc09',
         'university_name': 'National University',
-        'university_image': f'../assets/university_image//9855d5ef-16a1-4ed0-b322-0426791efc09.jpg',
+        'university_image': f'../assets/university_image/9855d5ef-16a1-4ed0-b322-0426791efc09.jpg',
         'university_intro': 'The National University of Bangladesh is a public university of Bangladesh that was established by an Act of Parliament '
                             'as an affiliating University of the country to impart graduate and post-graduate level education to the students through '
                             'its affiliated colleges and professional institutions throughout the country. It is the fourth largest university in the '
@@ -107,7 +107,7 @@ university = [
     {
         'university_id': '932477b6-3824-4214-9ca6-951a7ace8d7e',
         'university_name': 'Bangladesh Agricultural University',
-        'university_image': f'../assets/university_image//932477b6-3824-4214-9ca6-951a7ace8d7e.jpg',
+        'university_image': f'../assets/university_image/932477b6-3824-4214-9ca6-951a7ace8d7e.jpg',
         'university_intro': 'Bangladesh Agricultural University, also known as BAU, is a public agricultural university located in Mymensingh.'
                             ' Crop science, soil science, animal science, fisheries, and agricultural engineering are a few of the study areas linked to '
                             'agriculture that the institution offers undergraduate and graduate programs in.'
@@ -115,6 +115,34 @@ university = [
 ]
 
 admission_group = [
+    {
+        'admission_group_id': uuid4(),
+        'university_id': 'f9606f18-52c2-4664-8f1c-be2db74a9e17',
+        'group_name': 'Group-A',
+        'exam_datetime': random_date().strftime('%d %B, %Y'),
+        'application_fee': random.choice(application_fee)
+    },
+    {
+        'admission_group_id': uuid4(),
+        'university_id': 'f9606f18-52c2-4664-8f1c-be2db74a9e17',
+        'group_name': 'Group-B',
+        'exam_datetime': random_date().strftime('%d %B, %Y'),
+        'application_fee': random.choice(application_fee)
+    },
+    {
+        'admission_group_id': uuid4(),
+        'university_id': 'f9606f18-52c2-4664-8f1c-be2db74a9e17',
+        'group_name': 'Group-C',
+        'exam_datetime': random_date().strftime('%d %B, %Y'),
+        'application_fee': random.choice(application_fee)
+    },
+    {
+        'admission_group_id': uuid4(),
+        'university_id': 'f9606f18-52c2-4664-8f1c-be2db74a9e17',
+        'group_name': 'Group-D',
+        'exam_datetime': random_date().strftime('%d %B, %Y'),
+        'application_fee': random.choice(application_fee)
+    },
     {
         'admission_group_id': uuid4(),
         'university_id': 'cddf733e-6e9c-45af-8553-cc266e6aca2d',
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import datetime
 import json
+import os
 import uuid
 
 import requests as requests
 from flask import Flask, render_template, request, redirect, url_for
 
 from database import session
-from database.models import University, AdmissionGroup, Applicants
+from database.models import University, AdmissionGroup, Applicants, Payment
 
 app = Flask(__name__, template_folder='front-end/templates', static_folder='front-end/assets')
 
@@ -43,7 +45,112 @@ def university(applicant_id):
         }
 
         data.append(temp)
-    return render_template('list_uni.html', data=data)
+    return render_template('list_uni.html', data=data, applicant_id=applicant_id)
+
+
+@app.route("/pay")
+def pay():
+    applicant_id = request.args.get('applicant_id')
+    data = request.args.get('data')
+    data = json.loads(data)
+
+    total_amount = 0
+
+    for i in data:
+        amount = session.query(AdmissionGroup).filter_by(admission_group_id=i).first()
+        total_amount += amount.application_fee
+
+    tr = uuid.uuid4()
+    payment_data = {
+        'currency': 'BDT',
+        'shipping_method': 'NO',
+        'product_category': 'donation',
+        'product_profile': 'general',
+        'cus_postcode': 1200,
+        'cus_add1': 'Dhaka',
+        'cus_city': 'Dhaka',
+        'store_id': 'admis6422b090c562a',
+        'store_passwd': 'admis6422b090c562a@ssl',
+        'success_url': f'http://127.0.0.1:5000/pay-success?tr={tr}',
+        'fail_url': f'http://127.0.0.1:5000/pay-failed?tr={tr}',
+        'cancel_url': f'http://127.0.0.1:5000/pay-cancel?tr={tr}',
+        'tran_id': tr,
+        'cus_name': 'halum',
+        'cus_email': 'halum@admin.com',
+        'cus_phone': '0155665220',
+        'cus_country': 'BDT',
+        'product_name': 'Admission Payment',
+        'total_amount': str(total_amount)
+    }
+
+    payment = requests.post(
+        'https://sandbox.sslcommerz.com/gwprocess/v4/api.php',
+        data=payment_data
+    )
+
+    payment_response = payment.json()
+
+    session.add(Payment(**{
+        'transaction_id': tr,
+        'applicant_id': applicant_id,
+        'student_id': applicant_id,
+        'payment_amount': payment_data['total_amount'],
+        'payment_status': 'PENDING',
+        'payment_time': datetime.datetime.now()
+    }))
+
+    try:
+        session.commit()
+    except Exception:
+        session.rollback()
+
+    return redirect(payment_response['redirectGatewayURL'], code=302)
+
+
+@app.route("/std-login")
+def std_login():
+    return render_template('LoginStu.html')
+
+
+@app.route("/uni-login")
+def uni_login():
+    return render_template('LoginUni.html')
+
+
+@app.route("/pay-success", methods=['POST'])
+def pay_success():
+    transaction_id = request.args.get('tr')
+
+    session.query(Payment).filter_by(
+        transaction_id=transaction_id
+    ).update(
+        {'payment_status': 'DONE'}
+    )
+
+    try:
+        session.commit()
+    except Exception:
+        session.rollback()
+
+    return render_template('paySuccess.html')
+
+
+@app.route("/pay-failed", methods=['POST'])
+def pay_failed():
+    transaction_id = request.args.get('tr')
+
+    session.query(Payment).filter_by(
+        transaction_id=transaction_id
+    ).update(
+        {'payment_status': 'FAILED'}
+    )
+
+    try:
+        session.commit()
+    except Exception:
+        session.rollback()
+
+    return render_template('payFailed.html')
 
 
 @app.route("/confirm-form")
@@ -103,53 +210,12 @@ def create_user():
     return redirect(url_for('university', applicant_id=applicant_id))
 
 
-@app.route("/pay")
-def pay():
-    data = request.args.get('data')
-    data = json.loads(data)
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    os.makedirs('applicant_image', exist_ok=True)
 
-    total_amount = 0
+    applicant_id = request.args.get('fn')
+    image = request.files['image']
 
-    for i in data:
-        amount = session.query(AdmissionGroup).filter_by(admission_group_id=i).first()
-        total_amount += amount.application_fee
-
-    payment_data = {
-        'currency': 'BDT',
-        'shipping_method': 'NO',
-        'product_category': 'donation',
-        'product_profile': 'general',
-        'cus_postcode': 1200,
-        'cus_add1': 'Dhaka',
-        'cus_city': 'Dhaka',
-        'store_id': 'admis6422b090c562a',
-        'store_passwd': 'admis6422b090c562a@ssl',
-        'success_url': f'http://127.0.0.1:5000/pay-success',
-        'fail_url': f'http://127.0.0.1:5000/pay-failed',
-        'cancel_url': f'http://127.0.0.1:5000/pay-cancel',
-        'tran_id': uuid.uuid4(),
-        'cus_name': 'halum',
-        'cus_email': 'halum@admin.com',
-        'cus_phone': '0155665220',
-        'cus_country': 'BDT',
-        'product_name': 'Admission Payment',
-        'total_amount': str(total_amount)
-    }
-
-    payment = requests.post(
-        'https://sandbox.sslcommerz.com/gwprocess/v4/api.php',
-        data=payment_data
-    )
-
-    payment_response = payment.json()
-    return redirect(payment_response['redirectGatewayURL'], code=302)
-
-
-@app.route("/std-login")
-def std_login():
-    return render_template('LoginStu.html')
-
-
-@app.route("/uni-login")
-def uni_login():
-    return render_template('LoginUni.html')
+    image.save(f'applicant_image/{applicant_id}')
+    return 'Image uploaded successfully!'
