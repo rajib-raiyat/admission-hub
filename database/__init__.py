ae58b506d9423d8640db9279834f4e5917a7982e
Md. Monjor Morshed <2020-1-60-227@std.ewubd.edu>
2023-05-09 16:32:29 +0600

    Admit Card Picture and List_Uni updated

--- database/__init__.py
+++ database/__init__.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # Create a PostgreSQL database engine
-engine = create_engine('postgresql+psycopg2://ps_user:ps_pass@localhost:5432/admission_hub_db')
+engine = create_engine('postgresql+psycopg2://postgres:admin@localhost:5432/admission_hub_db')
 
 # Create a Session class to handle database interactions
 Session = sessionmaker(bind=engine)
